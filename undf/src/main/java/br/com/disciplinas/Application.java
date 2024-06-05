b48e7856e684ead1f4779cc1335d7fcc4cfc3420
Luiz Gustavo <142899763+1917dc@users.noreply.github.com>
2024-06-05 19:16:07 -0300

    Maven configurado

---
+++ undf/src/main/java/br/com/disciplinas/Application.java
@@ -0,0 +1,8 @@
+package br.com.disciplinas;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ undf/src/test/java/br/com/disciplinas/ApplicationTest.java
@@ -0,0 +1,13 @@
+package br.com.disciplinas;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ApplicationTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
